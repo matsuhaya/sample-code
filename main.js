aadfec4a0b0c788929b567082d36234914bceda7
matsuhaya <matsuhaya007@gmail.com>
2020-03-05 17:29:42 +0900

    1/2でErrorを出力するasync関数を追加

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
+const resolveSample = async () => {
+  if (Math.random() < 0.5) {
+    throw new Error('reject!!');
+  }
+  return 'resolve!!';
+};
 
+resolveSample()
+  .then(value => {
+    console.log(value);
+  })
+  .catch(err => {
+    console.log(err);
+  });
