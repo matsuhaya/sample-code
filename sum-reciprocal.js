9a336eee44119b3dd530ad729c0fc8be3a042a37
matsuhaya <matsuhaya007@gmail.com>
2020-03-17 23:14:45 +0900

    sum-reciprocal.jsの追加
    下記を参考にした
    https://s8a.jp/do-not-cause-stack-overflow-at-js

---
+++ sum-reciprocal.js
@@ -0,0 +1,25 @@
+// 逆数の総和がNを超える項は何番目か
+// 再帰関数で解くとスタックオーバーフローするので非同期処理で対応した例
+
+let answer = 0;
+let number = 1;
+
+const cal = answer => {
+  if (answer > 12) {
+    console.log(answer);
+    console.log(number - 1);
+    return;
+  } else {
+    answer += 1 / number;
+    number++;
+    // Stack Overflowする
+    // cal(answer);
+
+    // Stack Overflowの回避策
+    setTimeout(function() {
+      cal(answer);
+    }, 0);
+  }
+};
+
+cal(answer);
